e74802eabce50adf122aca65b5539b89c302eb78
omgshalihin <shalihinsgp@gmail.com>
2023-03-03 15:34:25 +0100

    get params from dynamic routing slug convention

--- app/deposit/[id]/page.tsx
+++ app/deposit/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { usePathname } from "next/navigation";
 import useSWR from "swr";
 import Link from "next/link";
 import Spinners from "@/app/components/Spinners";
@@ -10,9 +9,8 @@ import Deposit from "@/app/components/Deposit";
 const fetcher = (url: RequestInfo | URL) =>
   fetch(url).then((res) => res.json());
 
-const page = () => {
-  const pathName = usePathname();
-  const id = pathName?.split("/")[2];
+const page = ({ params }: { params: { id: string } }) => {
+  const id = params.id;
 
   const { data, error, isLoading } = useSWR(
     `http://localhost:8080/api/users/${id}`,
