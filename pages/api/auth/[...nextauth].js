3fcc5c3b23dd2bad6470fc794172459e55338ad6
omgshalihin <shalihinsgp@gmail.com>
2023-03-05 12:00:36 +0100

    removed authentication consent

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,13 +7,6 @@ export const authOptions = {
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        authorization: {
-          params: {
-            prompt: "consent",
-            access_type: "offline",
-            response_type: "code"
-          }
-        }
       })
   ],
   secret: process.env.JWT_SECRET
