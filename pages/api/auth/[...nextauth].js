a7e0de532a8ccf7e8eaa531a3a22187e6ebe00d4
omgshalihin <shalihinsgp@gmail.com>
2023-03-05 14:29:13 +0100

    refresh google tokens

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,6 +9,19 @@ export const authOptions = {
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       })
   ],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    })
+  ],
   secret: process.env.JWT_SECRET
 }
 
